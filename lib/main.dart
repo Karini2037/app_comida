74ec87707135556a85f71530a7fb3466caef3e30
Karini Sousa <131774110+Karini2037@users.noreply.github.com>
2025-03-19 19:47:18 -0300

    criando tela de app

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,33 @@
 import 'package:flutter/material.dart';
-import 'screens/tabs_screen.dart';
-import 'screens/categories_meals_screen.dart';
-import 'screens/meal_detail_screen.dart';
-import 'screens/settings_screen.dart';
-import 'utils/app_routes.dart';
-import 'models/meal.dart';
-import 'models/settings.dart';
-import 'data/dummy_data.dart';
+import 'screens/categories_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Settings settings = Settings();
-  List<Meal> _availableMeals = dummyMeals;
-  final List<Meal> _favoriteMeals = [];
-
-  void _filterMeals(Settings settings) {
-    setState(() {
-      this.settings = settings;
-      _availableMeals = dummyMeals.where((meal) {
-        final filterGluten = settings.isGlutenFree && !meal.isGlutenFree;
-        final filterLactose = settings.isLactoseFree && !meal.isLactoseFree;
-        final filterVegan = settings.isVegan && !meal.isVegan;
-        final filterVegetarian = settings.isVegetarian && !meal.isVegetarian;
-        return !filterGluten &&
-            !filterLactose &&
-            !filterVegan &&
-            !filterVegetarian;
-      }).toList();
-    });
-  }
-
-  void _toggleFavorite(Meal meal) {
-    setState(() {
-      _favoriteMeals.contains(meal)
-          ? _favoriteMeals.remove(meal)
-          : _favoriteMeals.add(meal);
-    });
-  }
-
-  bool _isFavorite(Meal meal) {
-    return _favoriteMeals.contains(meal);
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+       debugShowCheckedModeBanner: false,
       title: 'Vamos Cozinhar?',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: Colors.pink,
-          secondary: Colors.amber,
-        ),
-        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+      theme: ThemeData( //configuração da AppBar
+        appBarTheme:  const AppBarTheme(
+          foregroundColor: Colors.amber,
+          color: Colors.pink,
+          centerTitle: true,),
+        canvasColor: const Color.fromARGB(255, 29, 28, 1), //para a tela não ficar tão branco 
         fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
-              headline6: const TextStyle(
+              titleLarge: const TextStyle(
                 fontSize: 20,
                 fontFamily: 'RobotoCondensed',
               ),
             ),
       ),
-      routes: {
-        AppRoutes.home: (ctx) => TabsScreen(_favoriteMeals),
-        AppRoutes.categoriesMeals: (ctx) =>
-            CategoriesMealsScreen(_availableMeals),
-        AppRoutes.mealDetail: (ctx) =>
-            MealDetailScreen(_toggleFavorite, _isFavorite),
-        AppRoutes.settings: (ctx) => SettingsScreen(settings, _filterMeals),
-      },
+      home: const CategoriesScreen(), //importando as categorias 
     );
   }
 }
